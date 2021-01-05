cd044fecdd36a997c1cd01c4a2eeea30af40e8f2
Zywy <piotr@zywolewski.eu>
2021-01-05 08:55:39 +0100

    Added password inputs and ended button animation

---
+++ main.js
@@ -0,0 +1,28 @@
+
+modifyedDocumet = document.querySelector('.FomrSubmitButton');
+letterSpacing = modifyedDocumet.style.letterSpacing;
+text = modifyedDocumet.value;
+
+document.querySelector('.FomrSubmitButton').addEventListener("transitionstart", ()=>
+{
+    modifyedDocumet.style.letterSpacing='-5px';
+    
+    function makeStripes()
+    {
+         
+        textStripes="";
+        for(i=0;i<50;i++)
+        {
+            textStripes+= '-';
+        }
+        return textStripes
+    }
+    modifyedDocumet.value = makeStripes();
+});
+
+modifyedDocumet.addEventListener("transitionend", ()=>
+{
+    modifyedDocumet.value = text;
+    modifyedDocumet.style.letterSpacing=letterSpacing;
+});
+
